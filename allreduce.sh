678382909e61476d5e3aba28d4f50f83bfb4a1ef
Jessie Yang <jiaxiyan@amazon.com>
2024-01-24 16:06:07 -0800

    Allreduce tuning

--- allreduce.sh
+++ allreduce.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
 
-export HOME=/fsx
 cd ~
 
-[ -d libfabric ] || git clone https://github.com/ofiwg/libfabric.git -b main
-pushd libfabric
-./autogen.sh
-./configure --prefix=${HOME}/libfabric/install --disable-verbs --disable-psm3 --disable-opx --disable-usnic --disable-rstream --enable-efa
-make -j install
-popd
-
-[ -d ompi ] || git clone --recursive https://github.com/jiaxiyan/ompi.git -b allreduce
-pushd ompi
-./autogen.pl
-./configure --prefix=/fsx/ompi/install --with-libfabric=/fsx/libfabric/install --with-libevent=internal --with-hwloc=internal
-make -j install
-popd
-
-[ -d osu-micro-benchmarks ] || git clone https://github.com/wenduwan/osu-micro-benchmarks.git -b distribution
+[ -d osu-micro-benchmarks ] || wget -O osu-micro-benchmarks.tar.gz https://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-7.3.tar.gz
+tar -xvf osu-micro-benchmarks.tar.gz
 pushd osu-micro-benchmarks
-autoreconf -f -i
 ./configure --prefix=${HOME}/osu-micro-benchmarks/install CC=${HOME}/ompi/install/bin/mpicc CXX=${HOME}/ompi/install/bin/mpicxx
 make -j install
 popd
 
-/fsx/ompi/install/bin/mpirun --hostfile /fsx/PortaFiducia/hostfile --bind-to core -x LD_LIBRARY_PATH=/fsx/libfabric/install/lib:/fsx/ompi/install/lib -x PATH=/fsx/ompi/install/bin --map-by ppr:64:node --mca coll_han_allreduce_use_algorithm simple /fsx/osu-micro-benchmarks/install/libexec/osu-micro-benchmarks/mpi/collective/osu_allreduce
+[ -d ompi-collectives-tuning ] || git clone https://github.com/open-mpi/ompi-collectives-tuning.git
+pushd ompi-collectives-tuning
+./run_and_analyze.sh -c config-allreduce --scheduler slurm
+popd
